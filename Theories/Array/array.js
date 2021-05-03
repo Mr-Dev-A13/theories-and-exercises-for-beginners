710c7511df5f56f7a0f9c2d53853c602ddac9ac0
Avik Sargsyan <gukasyan.spa@gmail.com>
2021-05-03 23:50:22 +0400

    feat(array): add array methods without explanation

---
+++ Theories/Array/array.js
@@ -0,0 +1,34 @@
+// ARRAY [], new Array()
+/**  
+ * 1. push()
+ * 2. pop()
+ * 3. unshift()
+ * 4. shift()
+ * 5. length()
+ * 6. for of
+ * 7. toString()
+ * 8. [] + 13
+ * 
+ * 9. .... Methods ....
+ * 10. delete
+ * 11. splice(start, deleteEnd, add)
+ * 12. slice(start, end)
+ * 13. concat(arg1, arg2)
+ * 14. indexOf(item, from)
+ * 15. lastIndexOf(item, from)
+ * 16. includes(item, from)
+ * 17. find(function(item, index, array)
+ * 18. forEach(function(item, index, array)
+ * 19. filter(function(item, index, array) new Array
+ * 20. map(function(item, index, array) new Array
+ * 21. sort(fn)
+ * 22. reverse()
+ * 23. split(delim) 
+ * 24. join(glue)
+ * 25. reduce(function(previousValue, item, index, array) new Array
+ * 26. isArray(value)
+ * 27. some(fn)
+ * 28. every(fn)
+ * 29. fill(value, start, end) 
+ * 30. copyWithin(target, copyStart, copyEnd)
+ */
